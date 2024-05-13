c07e4edd77edca0d25aaf2678decf5abf188d704
Wenchuan <111623150+WenchuanLiliZhao@users.noreply.github.com>
2024-05-13 22:15:30 +0800

    Update App.tsx

--- NBL-beta-app/src/App.tsx
+++ NBL-beta-app/src/App.tsx
@@ -1,18 +1,22 @@
-import { useState } from 'react'
+// 莉莉备忘：
+// 一下是 import 图片的方式，到时候会用到
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+
+import { useState } from 'react'
+import './App.css' // 这个东西之后不需要，而且我需要用 scss 而不是 css
 
 
 import { BrowserRouter, Routes, Route, } from "react-router-dom";
 
+// 莉莉备忘：以下是根页面，之后需要建立一个 page index 来统筹管理这些页面
 import Home from './Pages/Home'
 import About from './Pages/About'
 import NoPage from './Pages/NoPage';
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0) // 说实话我不知道这是什么，先留着再说了
 
   return (
     <>
@@ -27,6 +31,7 @@ function App() {
         </BrowserRouter>
       </div>
 
+      {/* 说实话我不知道这是什么，先留着再说了 */}
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
