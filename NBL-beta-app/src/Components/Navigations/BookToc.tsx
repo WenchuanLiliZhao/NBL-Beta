95bfb425f0f2772934033c818abbeb4c671283ef
Wenchuan <111623150+WenchuanLiliZhao@users.noreply.github.com>
2024-05-23 18:24:30 +0800

    toc

--- NBL-beta-app/src/Components/Navigations/BookToc.tsx
+++ NBL-beta-app/src/Components/Navigations/BookToc.tsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
 import "./BookToc.scss";
 
-export default function BookToc() {
+export default function BookToc(props: any) {
+  const toc = props.toc
+
   return (<>
-    
+    {Object.values(toc).map((item: any, index: any) => (
+      <div className="" key={`${item}${index}`}>
+        <Link to={`/${item().key}`}>
+          {item().title}
+        </Link>
+      </div>
+    ))}
   </>)
 }
--- NBL-beta-app/src/Pages/DataBase/Books/TestAudioBook/Chapters/0_Layout.tsx
+++ NBL-beta-app/src/Pages/DataBase/Books/TestAudioBook/Chapters/0_Layout.tsx
@@ -1,8 +1,10 @@
 
 import AudioBookReadingLayout from "../../../../../Components/Layouts/AudioBookReadingLayout";
 import BookToc from "../../../../../Components/Navigations/BookToc";
+import Toc from "./0_Toc";
 
 export default function Layout(props: any) {
+
   return (<>
     <AudioBookReadingLayout audio={props.audio}>
       {props.children}
@@ -10,7 +12,7 @@ export default function Layout(props: any) {
       {/* toc 寫在這個地方，因為並不是所有書都有必要有目錄 */}
       {/* 實在不行最後封裝回去也可以 */}
 
-      <BookToc />
+      <BookToc toc={Toc}/>
     </AudioBookReadingLayout>
   </>)
 }
