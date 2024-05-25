338c6e17f2f38a35bf8dd965f5d88f055caa343c
Wenchuan <111623150+WenchuanLiliZhao@users.noreply.github.com>
2024-05-25 16:12:03 +0800

    test

--- NBL-beta-app/src/Pages/Inst/Riverwood/Pages/Home/Content.tsx
+++ NBL-beta-app/src/Pages/Inst/Riverwood/Pages/Home/Content.tsx
@@ -1,9 +1,13 @@
 
+
+import InstHeader from "../../../../../Components/PageSections/InstHeader";
+import InstInfo from "../../InstInfo";
 import RiverwoodLayout from "../../Layout";
 
 export default function Content() {
   return (<>
     <RiverwoodLayout>
+      <InstHeader instInfo={InstInfo} />
       Riverwood Home
     </RiverwoodLayout>
   </>)
