ccb7bb1f45343a4764bf0e3dec00e4e2ab1c74d5
Wenchuan <111623150+WenchuanLiliZhao@users.noreply.github.com>
2024-05-30 14:22:47 +0800

    updating 吶喊

--- NBL-beta-app/src/Pages/Books/NaHan_LuXun/TOC.tsx
+++ NBL-beta-app/src/Pages/Books/NaHan_LuXun/TOC.tsx
@@ -1,11 +1,22 @@
 import EP_KuangRenRiJi_NaHan from "./EPs/EP_KuangRenRiJi_NaHan/0_Repo";
+import EP_TuHeMao_NaHan from "./EPs/EP_TuHeMao_NaHan/0_Repo";
 import EP_YiJianXiaoShi_NaHan from "./EPs/EP_YiJianXiaoShi_NaHan/0_Repo";
+import EP_ZiXu_NaHan from "./EPs/EP_ZiXu_NaHan/0_Repo";
 
 const TOC = [
   {
     eps: [
-      EP_YiJianXiaoShi_NaHan,
+      EP_ZiXu_NaHan,
+
       EP_KuangRenRiJi_NaHan,
+
+      // 
+
+      EP_YiJianXiaoShi_NaHan,
+
+      // 
+      
+      EP_TuHeMao_NaHan,
     ]
   },
 
