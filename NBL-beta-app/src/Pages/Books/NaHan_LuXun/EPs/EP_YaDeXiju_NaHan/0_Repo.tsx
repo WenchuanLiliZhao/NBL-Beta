663e35683ea6752db10e157247400b3549ef60a9
Wenchuan <111623150+WenchuanLiliZhao@users.noreply.github.com>
2024-05-30 15:18:35 +0800

    update 吶喊

--- NBL-beta-app/src/Pages/Books/NaHan_LuXun/EPs/EP_YaDeXiju_NaHan/0_Repo.tsx
+++ NBL-beta-app/src/Pages/Books/NaHan_LuXun/EPs/EP_YaDeXiju_NaHan/0_Repo.tsx
@@ -3,9 +3,9 @@ import EPInfo from "./EPInfo";
 
 // https://zh.wikisource.org/wiki/Category:%E5%90%B6%E5%96%8A
 
-const EP_SheXi_NaHan = {
+const EP_KongYiJi_NaHan = {
   info: EPInfo,
   content: Content(),
 }
 
-export default EP_SheXi_NaHan
+export default EP_KongYiJi_NaHan
