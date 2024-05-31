0bef92cb0440ba724ee5110c00bbe14c8ba0c8ce
Wenchuan <111623150+WenchuanLiliZhao@users.noreply.github.com>
2024-05-31 21:46:57 +0800

    rename files

--- NBL-beta-app/src/Pages/Books/BookPages.tsx
+++ NBL-beta-app/src/Pages/Books/BookPages.tsx
@@ -2,7 +2,7 @@ import Book_TemplateBook from "./0_TemplateBook/0_Repo";
 import Book_Hamlet from "./Hamlet/0_Repo";
 import Book_Metamathematics from "./Metamathematics/0_Repo";
 import Book_Nahan_LuXun from "./NaHan_LuXun/0_Repo";
-import Book_TheSocialContract from "./The Social Contract/0_Repo";
+import Book_TheSocialContract from "./TheSocialContract/0_Repo";
 import Book_TheCelticTwilight from "./TheCelticTwilight/0_Repo";
 
 const BookPages = {
